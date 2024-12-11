7a8b4d6d48dd246f09071a3757f7192a7e4aecc2
hgs3 <82091+hgs3@users.noreply.github.com>
2024-12-11 10:57:07 -0600

    [skip ci] Verifying puncutation after links works.

--- tests/links/links.h
+++ tests/links/links.h
@@ -4,8 +4,14 @@
  *  Go review the documentation for the #nop function.
  *  
  *  Then go browse code on [GitHub](https://github.com) or another source hosting website.
+ *
+ *  For short code snipets, use [Gist](https://gist.github.com/).
+ *
  */
 
 /*! \brief Do nothing.
+ *
+ *  And if you don't like GitHub, there's always [BitBucket](https://bitbucket.org/),
+ *  [Codeberg](https://codeberg.org/), and [GitLab](https://en.wikipedia.org/wiki/GitLab)!
  */
 void nop(void);
